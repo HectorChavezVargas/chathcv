d3ddc24a93f3f0782530f29e7c893a5984a68163
HectorChavezVargas <monderjay@gmail.com>
2021-11-12 10:57:57 -0600

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDUxwW915KfM397ST0ZESCzW40NiYipXk8",
+  authDomain: "fbautenhcv.firebaseapp.com",
+  projectId: "fbautenhcv",
+  storageBucket: "fbautenhcv.appspot.com",
+  messagingSenderId: "713620515808",
+  appId: "1:713620515808:web:fffbd23f48305140a35985",
+  measurementId: "G-FFFGS32LH5"
+});
